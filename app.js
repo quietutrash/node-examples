7d8b409aadf28901565a3427b07c07c62c296441
Craig Connell <quietutrash@icloud.com>
2021-06-08 22:17:58 -0700

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+eonst rect = {
+    perimeter: (x, y) => 2 * (x + y),
+    area: (x, y) => x * y
+};
+
+function solveRect(l, w) {
+    console.log('Solving for rectangle with dimensions: ${l}, ${w}');
+    
+    if (l <= 0 || w) {
+        console.log('Rectangle dimensions must be greater than zero. Received: ${l}, ${w}');
+    } else {
+        console.log('Area of rectangle: ${rect.area(l, w)}');
+        console.log('Perimeter of rectangle: ${rect.perometer(l, w)}');
+    }
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(5, -3);
